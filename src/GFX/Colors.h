15212550ae6abcb64163832838b8de96cf99e975
Guilherme <gui-teixeirapimentel@hotmail.com>
2021-03-01 23:18:29 -0300

    Added colors helper (list of colors) to a namespace GFXColor

---
+++ src/GFX/Colors.h
@@ -0,0 +1,15 @@
+#ifndef COLORS_H
+#define COLORS_H
+#include "Graphics.h"
+
+namespace GFXColor
+{
+    constexpr Graphics::Color BLUE_COLOR = {0, 0, 255, 255};
+    constexpr Graphics::Color RED_COLOR = {255, 0, 0, 255};
+    constexpr Graphics::Color GREEN_COLOR = {0, 255, 0, 255};
+    constexpr Graphics::Color LIGHTBLUE_COLOR = {95, 154, 250, 255};
+    constexpr Graphics::Color LIGHTRED_COLOR = {214, 86, 86, 255};
+    constexpr Graphics::Color LIGHTGREEN_COLOR = {86, 214, 129, 255};
+}
+
+#endif
